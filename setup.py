604a9366840b5e10115b3a40df9c7f9ac8b99b1e
Mark <github@markedmondson.me>
2023-02-13 14:48:53 +0100

    fix depedencies

--- setup.py
+++ setup.py
@@ -25,11 +25,11 @@ release_status = (
 )
 dependencies = [
     "click>=7.0",
-    "google-auth==1.30.*",
-    "google-cloud-orchestration-airflow>=1.2.*",
-    "google-cloud-artifact-registry>=1.2.*",
+    "google-auth==1.30.2",
+    "google-cloud-orchestration-airflow>=1.2.0",
+    "google-cloud-artifact-registry>=1.2.0",
     "rich_click==1.4.0",
-    "docker==6.*",
+    "docker==6.0.1",
 ]
 extras = {
     "tests": ["pytest", "nox", "coverage"],
